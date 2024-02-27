7fb3244674d061a242d8e8d285550bbd14f2d3af
Subhodeep Paul <126082765+Subhodeeppaul@users.noreply.github.com>
2024-02-28 01:25:14 +0530

    joymb

---
+++ Assets/Assets/Scripts/SettingsPage.cs
@@ -0,0 +1,80 @@
+using UnityEngine.UIElements;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class SettingsPage : MonoBehaviour
+{
+    public UIDocument uidoc;
+
+    VisualElement setting;
+    public Button setbut;
+
+    VisualElement gameplayPage;
+    VisualElement cameraPage;
+    VisualElement audioPage;
+    VisualElement accessibilityPage;
+    VisualElement viewControlPage;
+    VisualElement changeControlPage;
+    VisualElement creditsPage;
+
+    void Start()
+    {
+        VisualElement root = uidoc.rootVisualElement;
+        setting = root.Q<VisualElement>("SettingsPage");
+
+        gameplayPage = root.Q<VisualElement>("GamePlayPage");
+        cameraPage = root.Q<VisualElement>("CameraPage");
+        audioPage = root.Q<VisualElement>("AudioPage");
+        accessibilityPage = root.Q<VisualElement>("AccessibilityPage");
+        viewControlPage = root.Q<VisualElement>("ViewControlPage");
+        changeControlPage = root.Q<VisualElement>("ChangeControlPage");
+        creditsPage = root.Q<VisualElement>("CreditsPage");
+
+        setbut = setting.Q<Button>("Gameplay");
+        setbut.RegisterCallback<ClickEvent>(c =>
+        {
+            ShowPageAndHideOthers(gameplayPage);
+        });
+        RegisterButton(setting.Q<Button>("Camera"), cameraPage);
+        RegisterButton(setting.Q<Button>("Audio"), audioPage);
+        RegisterButton(setting.Q<Button>("Accessibility"), accessibilityPage);
+        RegisterButton(setting.Q<Button>("ViewControl"), viewControlPage);
+        RegisterButton(setting.Q<Button>("ChangeControl"), changeControlPage);
+        RegisterButton(setting.Q<Button>("Credits"), creditsPage);
+    }
+
+    void RegisterButton(Button button, VisualElement page)
+    {
+        button.RegisterCallback<ClickEvent>(c =>
+        {
+            ShowPageAndHideOthers(page);
+        });
+    }
+
+    void ShowPageAndHideOthers(VisualElement pageToShow)
+    {
+        gameplayPage.style.display = DisplayStyle.None;
+        cameraPage.style.display = DisplayStyle.None;
+        audioPage.style.display = DisplayStyle.None;
+        accessibilityPage.style.display = DisplayStyle.None;
+        viewControlPage.style.display = DisplayStyle.None;
+        changeControlPage.style.display = DisplayStyle.None;
+        creditsPage.style.display = DisplayStyle.None;
+
+        pageToShow.style.display = DisplayStyle.Flex;
+        setting.style.display = DisplayStyle.None;
+    }
+
+    public void SetDisplay(bool isDisplaybool)
+    {
+        if (isDisplaybool)
+        {
+            setting.style.display = DisplayStyle.Flex;
+        }
+        else
+        {
+            setting.style.display = DisplayStyle.None;
+        }
+    }
+}
